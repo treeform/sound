519ecfe8dbe780f1991d7b4c51eb40266537f136
treeform <starplant@gmail.com>
2018-08-29 07:44:18 -0700

    test changes

--- tests/test.nim
+++ tests/test.nim
@@ -3,13 +3,13 @@ import sound
 
 
 block:
-  # playing wav file
+  echo "playing wav file"
   let sound = newSoundWithFile("tests/ding.wav")
   sound.play()
   sleep(1000)
 
 block:
-  # playing ogg file
+  echo "playing ogg file"
   let sound = newSoundWithFile("tests/robo.ogg")
   sound.play()
   sleep(1000)
@@ -17,13 +17,18 @@ block:
 block:
   # playing sound in 3d
   let sound = newSoundWithFile("tests/drums.mono.wav")
-  # play on the right
+  echo "playing on the right"
   var source = sound.playWithSource()
   source.setPos(1,0,0)
-  sleep(1000)
-  # play on the left
+  sleep(2500)
+  source.stop()
+
+  sleep(500)
+
+  echo "playing on the left"
+  source = sound.playWithSource()
   source.setPos(-1,0,0)
-  sleep(1000)
+  sleep(2500)
   source.stop()
 
 block:
@@ -32,8 +37,10 @@ block:
   sound.setLooping(true)
   var source = sound.playWithSource()
   # make 2 rounds
+  echo "rotateing sound in 3d, 2 rotations"
   for i in 0..360 * 2:
     let a = float(i) / 180 * PI
     source.setPos(sin(a), cos(a), 0)
-    sleep(10)
+    sleep(20)
   source.stop()
+  sleep(500)
